5b03ef2663b7c4b4b339399ccf1675f218bedd0b
jaemak23 <macklin.jaison@gmail.com>
2022-06-07 12:14:32 +0530

    Added base_url and api_key to string resource file.

--- app/src/main/java/com/jaisonmacklin/test/marsroverphotogallery/data/api/RetrofitService.kt
+++ app/src/main/java/com/jaisonmacklin/test/marsroverphotogallery/data/api/RetrofitService.kt
@@ -1,5 +1,6 @@
 package com.jaisonmacklin.test.marsroverphotogallery.data.api
 
+import com.jaisonmacklin.test.marsroverphotogallery.R
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -7,10 +8,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitService {
 
     var client:OkHttpClient=OkHttpClient.Builder().build()
-    private const val BASE_URL="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/"
+    //private const val BASE_URL="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/"
 
     private var retrofit=Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(R.string.base_url.toString())
         .addConverterFactory(GsonConverterFactory.create())
         .client(client)
         .build()
