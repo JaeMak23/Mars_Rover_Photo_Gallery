c58b924fac7ebda109b2eb733a2f39059d6594f7
jaemak23 <macklin.jaison@gmail.com>
2022-06-07 12:37:24 +0530

    Added dataBinding to build.gradle(app) & Moved MainActivity.kt to views package

--- app/src/main/java/com/jaisonmacklin/test/marsroverphotogallery/views/MainActivity.kt
+++ app/src/main/java/com/jaisonmacklin/test/marsroverphotogallery/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jaisonmacklin.test.marsroverphotogallery
+package com.jaisonmacklin.test.marsroverphotogallery.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jaisonmacklin.test.marsroverphotogallery.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
